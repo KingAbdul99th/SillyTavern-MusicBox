001c8285f7662cc8fa447e2f2d2335860c38e8ee
KingAbdul99th <adadreigh@gmail.com>
2025-05-14 06:25:18 +0300

    cleanup imports

--- src/Settings.tsx
+++ src/Settings.tsx
@@ -1,48 +1,17 @@
 import React, { useState } from "react";
-import { getExtensionSettings, setExtensionSettings } from "./ExtensionSettings.js";
-import MusicPlayer from "./MusicPlayer.js";
-import YouTubeMusicPlayer from "./YoutubeMusicPlayer.js";
-
-function toggleRemoveExtrasFromExtensionsBlock(removeExtras: boolean) {
-  const extensionsBlock = document.getElementById("rm_extensions_block");
-  const parent = extensionsBlock?.children[0];
-  const children = parent?.children as HTMLCollectionOf<HTMLElement>;
-  const child1 = children[4];
-  const child2 = children[5];
-
-  if(removeExtras) {
-    child1.style.display = "none";
-    child2.style.display = "none";
-  }
-  else {
-    child1.style.display = "";
-    child2.style.display = "";
-  }
-}  
+import { getExtensionSettings, setExtensionSettings } from "./ExtensionSettings";
+import MusicPlayer from "./MusicPlayer";
+import YouTubeMusicPlayer from "./YoutubeMusicPlayer";
 
 export default function Settings() {
   let extensionSettings = getExtensionSettings();
   const [enabled, setEnabled] = useState(extensionSettings.enabled);
-  const [removeExtras, setRemoveExtras] = useState(extensionSettings.removeExtras);
   const [musicVideoId, setMusicVideoId] = useState('dQw4w9WgXcQ');
   
-  if(enabled) {
-    toggleRemoveExtrasFromExtensionsBlock(extensionSettings.removeExtras);
-  }
-
   async function handleEnabledClick() {
     extensionSettings = setExtensionSettings({...extensionSettings, enabled: !extensionSettings.enabled});
     console.log("enable toggled ", extensionSettings.enabled);
     setEnabled(extensionSettings.enabled);
-  }
-
-  async function handleRemoveExtrasClick() {
-    extensionSettings = setExtensionSettings({...extensionSettings, removeExtras: !extensionSettings.removeExtras});
-    console.log("removeExtras toggled ", extensionSettings.removeExtras);
-    setRemoveExtras(extensionSettings.removeExtras);
-    if(enabled) {
-      toggleRemoveExtrasFromExtensionsBlock(extensionSettings.removeExtras);
-    }
   }
 
   return (
@@ -56,8 +25,6 @@ export default function Settings() {
           <div className="music-box-block flex-container">
             <input id="music-box-enable" type="checkbox" onClick={handleEnabledClick} checked={enabled}/>
             <label htmlFor="music-box-enable">Enable music-box</label>
-            <input id="music-box-remove-extras" type="checkbox" onClick={handleRemoveExtrasClick} checked={removeExtras}/>
-            <label htmlFor="music-box-remove-extras">Remove Extras Block</label>
           </div>
           <hr className="sysHR" />
           <MusicPlayer audioUrl={"https://www.soundhelix.com/examples/mp3/SoundHelix-Song-9.mp3"} />
